9784b13779ac9e9551c277fd3e21cb4eb7205905
hogg21 <travisjones263@gmail.com>
2021-08-22 00:26:09 +0300

    second change

--- index.js
+++ index.js
@@ -4,19 +4,22 @@ const h1 = document.querySelector('h1');
 
 let count = 0;
 h1.innerText = count;
+function render() {
+    h1.innerText = count;
+    if (count > 0) {
+        MinusBtn.removeAttribute('disabled', '');
+    } else{
+        MinusBtn.setAttribute('disabled', '');
+    }
+};
 
 PlusBtn.addEventListener('click', () => {
     count++;
-    h1.innerText = count;
-    if (!count) {
-        MinusBtn.removeAttribute('disabled', '');
-    }
+    render();
+
 })
 
 MinusBtn.addEventListener('click', () => {
     count--;
-    h1.innerText = count;
-    if (!count) {
-        MinusBtn.setAttribute('disabled', '');
-    }
+   render();
 })
